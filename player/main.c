c80703c83e88504548eb34acd0af24b4e56a86bc
Benjamin Otte <otte@gnome.org>
2007-12-12 11:46:56 +0100

    check in skeleton
    it's woring properly, but not doing anything useful

---
+++ player/main.c
@@ -0,0 +1,55 @@
+/* Swfdec
+ * Copyright (C) 2007 Benjamin Otte <otte@gnome.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <libswfdec/swfdec.h>
+
+int 
+main (int argc, char *argv[])
+{
+  GError *error = NULL;
+
+  static const GOptionEntry options[] = {
+    { NULL }
+  };
+  GOptionContext *ctx;
+
+  ctx = g_option_context_new ("");
+  g_option_context_add_main_entries (ctx, options, "options");
+  g_option_context_parse (ctx, &argc, &argv, &error);
+  g_option_context_free (ctx);
+
+  if (error) {
+    g_printerr ("Error parsing command line arguments: %s\n", error->message);
+    g_error_free (error);
+    return 1;
+  }
+
+  swfdec_init ();
+
+  if (argc < 2) {
+    g_printerr ("Usage: %s [OPTIONS] filename\n", argv[0]);
+    return 1;
+  }
+  
+  return 0;
+}
+
